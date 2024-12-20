62e77f1fc75e697c23ecd4fe865640b114afdf7f
cmillerl <cmi1779@wgu.edu>
2024-12-20 14:54:22 -0800

    Added a few variables to the truck class.

---
+++ Main.py
@@ -0,0 +1,5 @@
+#Main Class
+
+class Main:
+    def __init__(self):
+        v = 1
---
+++ Truck.py
@@ -0,0 +1,16 @@
+class Truck:
+    def __init__(self):
+        
+        self.avgMPH = (18/60)
+        self.currentDriver = None
+        self.maxDistance = 140
+        self.maxPackages = 16
+        self.packagesDelivered = None
+        self.packagesInTruck = None
+        self.truckStatus = None
+        self.totalMiles = None
+        self.totalTrucks = 3
+        self.truckNumber = None
+
+    
+
